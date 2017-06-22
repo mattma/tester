6952084b2356901bbcc155250094ebf372c58484
Matt Ma <bigmabig@gmail.com>
2017-06-21 22:26:08 -0700

    add system locale check

--- app/router.js
+++ app/router.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const osLocale = require('os-locale');
+const locale = osLocale.sync();
+// China zh_CN
+const defaultLocale = locale === 'en_US' ? '/zh-cn' : '/en';
+
 module.exports = app => {
   app.get('/zh-cn*', 'cn');
   app.get('/en*', 'en');
-  app.redirect('/', '/en');
+
+  // redirect based on User's system locale
+  app.redirect('/', defaultLocale);
+  // Catch all clause. Trigger when there is no match
+  app.get('(.*)', defaultLocale);
 };
